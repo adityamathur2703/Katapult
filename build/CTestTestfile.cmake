70a5937ecafdd7b053a77b5a225213e1dc6ac4e3
adityamathur2703 <mathuraditya.270302@gmail.com>
2024-08-17 22:13:00 +0530

    v1

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kpit/Desktop/TEST_KATAPULT/Day_2/Ques_1
-# Build directory: /home/kpit/Desktop/TEST_KATAPULT/Day_2/Ques_1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
